dffe08490beb4b9db163794035b706c1da2e6b23
Ousnik16 <kundurik@gmail.com>
2026-04-02 18:06:20 +1300

    "Commit"

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://127.0.0.1:3000",
+        "https://minicapstoneprojectnetwork1.kundurik758.workers.dev"
     ],
     allow_credentials=True,
     allow_methods=["*"],
